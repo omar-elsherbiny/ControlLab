a4f3acf51f1904a24415b7e2b63cbc7d07479a4e
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2026-03-25 18:10:13 +0200

    update folders

--- plotting.py
+++ plotting.py
@@ -1,6 +1,7 @@
 # plotting.py
 import matplotlib.pyplot as plt
-
+import control as ctrl
+import numpy as np
 
 def plot_time(t, y, title="Response"):
     plt.figure()
@@ -22,6 +23,22 @@ def plot_compare(t, signals, labels, title="Comparison"):
     plt.ylabel("Amplitude")
     plt.legend()
     plt.grid()
+    plt.show()
+
+def plot_pzmap(sys, title="Pole-Zero Map"):
+    plt.figure()
+    poles = ctrl.poles(sys)
+    zeros = ctrl.zeros(sys)
+
+    plt.scatter(np.real(poles), np.imag(poles), marker='x', color='r', label='Poles', s=100)
+    plt.scatter(np.real(zeros), np.imag(zeros), marker='o', facecolors='none', edgecolors='b', label='Zeros', s=100)
+    plt.axhline(0, color='k', linewidth=0.5)
+    plt.axvline(0, color='k', linewidth=0.5)
+    plt.xlabel('Re')
+    plt.ylabel('Im')
+    plt.title(title)
+    plt.legend()
+    plt.grid(True)
     plt.show()
 
 
